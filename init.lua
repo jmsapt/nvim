ed9e75b6d3840cf3770f3c32a7f45b6482c5afc8
James Appleton <z5310803@ad.unsw.edu.au>
2024-04-10 10:24:54 +1000

    saving laptop temp changes

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd([[
+    autocmd FileType c setlocal shiftwidth=4 softtabstop=4
+    autocmd FileType cpp setlocal shiftwidth=4 softtabstop=4
+]])
