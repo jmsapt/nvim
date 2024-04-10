1af30321ebec0eaee4ce61493a5dd676d5fde92d
James Appleton <z5310803@ad.unsw.edu.au>
2024-04-10 14:21:34 +1000

    added clipboard support for wsl

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -13,29 +13,6 @@ return {
             desc = "Format Injected Langs",
         },
     },
-    init = function()
-        -- Install the conform formatter on VeryLazy
-        LazyVim.on_very_lazy(function()
-            LazyVim.format.register({
-                name = "conform.nvim",
-                priority = 100,
-                primary = true,
-                format = function(buf)
-                    local plugin = require("lazy.core.config").plugins["conform.nvim"]
-                    local Plugin = require("lazy.core.plugin")
-                    local opts = Plugin.values(plugin, "opts", false)
-                    require("conform").format(LazyVim.merge({}, opts.format, { bufnr = buf }))
-                end,
-                sources = function(buf)
-                    local ret = require("conform").list_formatters(buf)
-                    ---@param v conform.FormatterInfo
-                    return vim.tbl_map(function(v)
-                        return v.name
-                    end, ret)
-                end,
-            })
-        end)
-    end,
     opts = function()
         local plugin = require("lazy.core.config").plugins["conform.nvim"]
 
