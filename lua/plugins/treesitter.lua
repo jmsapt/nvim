b53caaae668d8e829d209258e1a16efdf0d5f865
James Appleton <james.appleton01@gmail.com>
2025-08-13 18:00:15 +1000

    amended bug in treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,7 +10,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
       require('nvim-treesitter.configs').setup({ indent = { enable = true }, })
-      require("nvim-treesitter").setup({})
     end,
   },
 }
